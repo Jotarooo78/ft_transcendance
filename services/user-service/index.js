832092577550200edb8a0ba3182440e16c3a868d
armosnie <armosnie@f4r10s12.paris.42.school>
2026-09-04 14:36:26 +0200

    add : some dependancies in 2 files package; feat : rate limit in index.js

--- services/user-service/index.js
+++ services/user-service/index.js
@@ -2,24 +2,29 @@ import Fastify from 'fastify';
 import client from 'prom-client';
 import postgres from '@fastify/postgres';
 import jwt from '@fastify/jwt';
+import rateLimit from '@fastify/rate-limit';
 
 const app = Fastify({ logger: true });
-client.collectDefaultMetrics();
+
+client.collectDefaultMetrics(); //Prometheus collect
 
 app.register(postgres, { connectionString: process.env.DATABASE_URL });
 app.register(jwt, { secret: process.env.JWT_SECRET });
+app.register(rateLimit, {
+  global: false,
+});
 
-app.get('/health', async () => ({ status: 'ok', service: 'user-service' }));
+app.get('/health', async () => ({ status: 'ok', service: 'auth-service' })); //healthcheck
 
 app.get('/metrics', async (req, reply) => {
-  reply.header('Content-Type', client.register.contentType);
+  reply.header('Content-Type', client.register.contentType); // GPU, CPU info... etc
   return client.register.metrics();
 });
 
 app.get('/me', {
   onRequest: async (req, reply) => {
     try {
-      await req.jwtVerify(); // lit et vérifie le header Authorization: Bearer <token>
+      await req.jwtVerify();
     } catch {
       reply.code(401).send({ error: 'unauthorized' });
     }
@@ -36,4 +41,24 @@ app.get('/me', {
   return rows[0];
 });
 
-app.listen({ port: 4001, host: '0.0.0.0' });
+app.post('/login', {
+  config: {
+    rateLimit: {
+      max: 5,
+      timeWindow: '1 minute'
+    }
+  }
+}, async (req, reply) => {
+  const { email, password } = req.body;
+});
+
+const start = async () => { // server launch
+  try {
+    await app.listen({ port: 4001, host: '0.0.0.0' });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
